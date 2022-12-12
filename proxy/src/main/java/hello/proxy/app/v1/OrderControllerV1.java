55ae6c97bbdf3f01ff7ed03aad87c969ecd236a8
이예린 <>
2022-12-12 23:25:14 +0900

    proxy V1

---
+++ proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -0,0 +1,18 @@
+package hello.proxy.app.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping
+@ResponseBody
+public interface OrderControllerV1 {
+
+    @GetMapping("/v1/request")
+    String request(@RequestParam("itemId") String itemId);
+
+    @GetMapping
+    String noLog();
+
+}
---
+++ proxy/src/main/java/hello/proxy/app/v1/OrderRepositoryV1Impl.java
@@ -0,0 +1,20 @@
+package hello.proxy.app.v1;
+
+public class OrderRepositoryV1Impl implements OrderRepositoryV1{
+
+    @Override
+    public void save(String itemId) {
+        if(itemId.equals("ex")){
+            throw new IllegalArgumentException("예외발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
