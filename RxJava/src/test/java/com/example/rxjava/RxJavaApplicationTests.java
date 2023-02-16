c5675b6d666f13c4be4412c01f11c8abeb6a9285
이예린 <ebbunbul@gmail.com>
2023-02-16 21:04:54 +0900

    테스트 관련이랄가,,?

--- RxJava/src/test/java/com/example/rxjava/RxJavaApplicationTests.java
+++ RxJava/src/test/java/com/example/rxjava/RxJavaApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.rxjava;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest 가 없어도 테스트가 실행되는 이유 : Junit5
+@Slf4j
 class RxJavaApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() { //Junit5 는 public, private 가 없어도 됨
+        log.info("과연 테스트가 실행될것인가!!!");
     }
 
 }
